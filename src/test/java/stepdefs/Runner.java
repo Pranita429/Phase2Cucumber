20eb30c750400ef96ee1baffde77375b0d4ac757
your name <you@example.com>
2022-10-27 11:30:35 +0530

    ss commit

--- src/test/java/stepdefs/Runner.java
+++ src/test/java/stepdefs/Runner.java
@@ -10,9 +10,10 @@ import io.cucumber.junit.CucumberOptions;
 	@CucumberOptions(
 			
 			monochrome=true,
-		    plugin = {"pretty","html:target/html-cucumber","json:target/cucumber.json"}, 
+		    plugin = {"pretty","html:target/cucumber-html-report","json:target/cucumber.json"}, 
 			features="src/test/java/features",
 			glue="stepdefs",
+			
 			tags= {"@sanity or @regression"} 
 		
 			)
